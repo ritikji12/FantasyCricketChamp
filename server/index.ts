e01e44d51e5b923aed0f8c0357ba6a5ac396bb36
ritikji12 <itsritikdhankhar@gmail.com>
2025-04-22 16:18:31 +0530

    index.ts

--- server/index.ts
+++ server/index.ts
@@ -49,9 +49,8 @@ async function startServer() {
 }
 
 // Start server if this is the main module
-if (import.meta.url === import.meta.resolve('./index.ts')) {
-  startServer().catch(err => {
-    console.error("Failed to start server:", err);
-    process.exit(1);
-  });
-}
+// Start server immediately since we're using ES modules
+startServer().catch(err => {
+  console.error("Failed to start server:", err);
+  process.exit(1);
+});
